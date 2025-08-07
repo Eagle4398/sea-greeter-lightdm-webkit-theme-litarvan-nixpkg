7bc9d39aaf695c3c9a73c60810fdcdad995837e5
Eagle4398 <177400222+Eagle4398@users.noreply.github.com>
2025-08-07 11:37:32 +0200

    add background support as nixpkg + public domain backgrounds

--- default.nix
+++ default.nix
@@ -2,7 +2,10 @@
 let
   pkgs = import <nixpkgs> {};
   litarvan-theme = pkgs.callPackage ./litarvan-theme.nix {};
+  nature-images = pkgs.callPackage ./nature-images.nix {};
 in
   pkgs.callPackage ./sea-greeter.nix {
         theme = litarvan-theme;
+        backgrounds = nature-images;
   }
+
--- sea-greeter.nix
+++ sea-greeter.nix
@@ -1,5 +1,6 @@
 { lib, stdenv, fetchFromGitHub, meson, ninja, pkg-config, gtk3, webkitgtk
-, lightdm, glib, libyaml, typescript, fetchurl, theme ? null }:
+, lightdm, glib, libyaml, typescript, fetchurl, theme ? null, backgrounds ? null
+}:
 
 stdenv.mkDerivation rec {
   pname = "sea-greeter";
@@ -47,35 +48,40 @@ stdenv.mkDerivation rec {
   '';
 
   installPhase = ''
-        meson install -C build --destdir=$out
+    meson install -C build --destdir=$out
 
-        # So, for some reason, if I either don't do destdir=$out or prefix=$out for
-        # the build, what happens is that either the bin/ or the config files copied
-        # over from the setup don't appear in the final nix pkg. This means the only
-        # way I've found to guarantee that every file is there to do both. This has
-        # the problem that part of the files is double-"indented", meaning that the
-        # nix store path is repeated with /nix/store/.../nix/store/.../bin. But
-        # these files you can just move.
-        NESTED_DIR=$(ls -d $out/nix/store/*)
-        shopt -s dotglob
-        mv $NESTED_DIR/* $out/
-        rm -rf $out/nix
+    # So, for some reason, if I either don't do destdir=$out or prefix=$out for
+    # the build, what happens is that either the bin/ or the config files copied
+    # over from the setup don't appear in the final nix pkg. This means the only
+    # way I've found to guarantee that every file is there to do both. This has
+    # the problem that part of the files is double-"indented", meaning that the
+    # nix store path is repeated with /nix/store/.../nix/store/.../bin. But
+    # these files you can just move.
+    NESTED_DIR=$(ls -d $out/nix/store/*)
+    shopt -s dotglob
+    mv $NESTED_DIR/* $out/
+    rm -rf $out/nix
 
-        # patch desktop executable reference
-        substituteInPlace $out/usr/share/xgreeters/sea-greeter.desktop \
-          --replace "Exec=sea-greeter" "Exec=$out/bin/sea-greeter"
+    # patch desktop executable reference
+    substituteInPlace $out/usr/share/xgreeters/sea-greeter.desktop \
+      --replace "Exec=sea-greeter" "Exec=$out/bin/sea-greeter"
 
-        # the xserver.lightdm.greeter.package options expects the .desktop file to 
-        # be on the root level. 
-        ln -s $out/usr/share/xgreeters/sea-greeter.desktop $out/sea-greeter.desktop
+    # the xserver.lightdm.greeter.package options expects the .desktop file to 
+    # be on the root level. 
+    ln -s $out/usr/share/xgreeters/sea-greeter.desktop $out/sea-greeter.desktop
 
-        ${
-          lib.optionalString (theme != null) ''
-            echo "Installing theme: ${theme.pname}"
-            local theme_dir="$out/usr/share/web-greeter/themes/${theme.pname}"
-            ln -s ${theme} "$theme_dir"
-          ''
-        }
+    ${lib.optionalString (theme != null) ''
+      echo "Installing theme: ${theme.pname}"
+      local theme_dir="$out/usr/share/web-greeter/themes/${theme.pname}"
+      ln -s ${theme} "$theme_dir"
+    ''}
+    ${lib.optionalString (backgrounds != null) ''
+      echo "Installing backgrounds from: ${backgrounds}" 
+      local backgrounds_dir="$out/usr/share/backgrounds"
+      mkdir -p "$backgrounds_dir"
+      ln -s "${backgrounds}"/* "$backgrounds_dir/"
+      ls "$backgrounds_dir"
+    ''}
   '';
 
   meta = with lib; {
